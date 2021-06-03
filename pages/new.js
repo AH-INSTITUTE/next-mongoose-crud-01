09990209493cf7ab003e4cf88617cd5ae7a1f405
AH-INSTITUTE <ahinstitutte@gmail.com>
2021-06-03 19:19:15 +0600

    change localhost to heroku and complete crud

--- pages/new.js
+++ pages/new.js
@@ -23,7 +23,7 @@ const NewNote = () => {
 
     const createNote = async () => {
         try {
-            const res = await fetch('http://localhost:3000/api/notes', {
+            const res = await fetch('https://next-moongoose-crud.herokuapp.com/api/notes', {
                 method: 'POST',
                 headers: {
                     "Accept": "application/json",
